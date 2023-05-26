a190a2784a50b045978a5996b642484ca9c997c4
Siva Kumar <sivakumar9399959803@gmail.com>
2023-05-26 19:45:14 +0530

    roboshop catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -23,7 +23,7 @@ echo -e "\e[35mInstall NodeJS Dependencies\e[0m"
 npm install  &>>/tmp/roboshop.log
 
 echo -e "\e[35mSetup Systemd Service\e[0m"
-cp /home/centos/roboshop-shell/catalogue service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
 
 echo -e "\e[35mStart Catalogue Service\e[0m"
 systemctl daemon-reload   &>>/tmp/roboshop.log
