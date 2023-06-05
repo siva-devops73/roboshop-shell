518cdeb869a036e58a8e215baf2d05fdf8f9e446
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-05 16:25:14 +0530

    roboshop common script

--- common.sh
+++ common.sh
@@ -38,3 +38,15 @@ nodejs() {
   systemctl restart ${component}  &>>${log_file}
 
 }
+
+mongo_schema_setup() {
+
+  echo -e "${color} Copy Mongo Repo file ${nocolor}"
+  cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+
+  echo -e "${color} Install MongoDB Client ${nocolor}"
+  yum install mongodb-org-shell -y  &>>${log_file}
+
+  echo -e "${color} Load Schema ${nocolor}"
+  mongo --host mongodb-dev.devops73.store <${app_path}/schema/${component}.js  &>>${log_file}
+}
