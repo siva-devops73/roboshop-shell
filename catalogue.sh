cda408dfc7f674e45054ef189fa90930be7b5d1f
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-05 16:25:53 +0530

    roboshop catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -3,11 +3,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} Copy Mongo Repo file ${nocolor}"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
-
-echo -e "${color} Install MongoDB Client ${nocolor}"
-yum install mongodb-org-shell -y  &>>${log_file}
-
-echo -e "${color} Load Schema ${nocolor}"
-mongo --host mongodb-dev.devops73.store <${app_path}/schema/${component}.js  &>>${log_file}
+mongo_schema_setup
