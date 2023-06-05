db7a0636a38a49f8aa50f1773d95a489f2bcf87a
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-05 09:54:05 +0530

    roboshop catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,5 @@
+component=${component}
+
 echo -e "\e[35mConfiguring NodeJS Repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
 
@@ -12,23 +14,23 @@ rm -rf /app  &>>/tmp/roboshop.log
 mkdir /app  &>>/tmp/roboshop.log
 
 echo -e "\e[35mDownload Application Content\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>/tmp/roboshop.log
 cd /app  &>>/tmp/roboshop.log
 
 echo -e "\e[35mExtract Application Content\e[0m"
-unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip  &>>/tmp/roboshop.log
 cd /app  &>>/tmp/roboshop.log
 
 echo -e "\e[35mInstall NodeJS Dependencies\e[0m"
 npm install  &>>/tmp/roboshop.log
 
 echo -e "\e[35mSetup Systemd Service\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>/tmp/roboshop.log
 
 echo -e "\e[35mStart Catalogue Service\e[0m"
 systemctl daemon-reload   &>>/tmp/roboshop.log
-systemctl enable catalogue  &>>/tmp/roboshop.log
-systemctl restart catalogue  &>>/tmp/roboshop.log
+systemctl enable ${component}  &>>/tmp/roboshop.log
+systemctl restart ${component}  &>>/tmp/roboshop.log
 
 echo -e "\e[35mCopy Mongo Repo file\e[0m"
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
@@ -37,4 +39,4 @@ echo -e "\e[35mInstall MongoDB Client\e[0m"
 yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
 
 echo -e "\e[35mLoad Schema\e[0m"
-mongo --host mongodb-dev.devops73.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devops73.store </app/schema/${component}.js  &>>/tmp/roboshop.log
