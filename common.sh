8d85be3318caa243f5130d9b4146b641044cb110
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 15:25:31 +0530

    roboshop common script

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
-color="\e[35m"
-nocolor="\e[0m"
-log_file="/tmp/roboshop.log"
+color="${color} "
+nocolor=" ${nocolor}"
+log_file="${log_file}"
 app_path="/app"
 
 
@@ -95,6 +95,20 @@ python() {
    echo -e "${color} Install Application Dependencies ${nocolor}"
    cd ${app_path}  &>>${log_file}
    pip3.6 install -r requirements.txt  &>>${log_file}
+
+   systemd_setup
+}
+
+golang() {
+   echo -e "${color} Install Golang ${nocolor}"
+   yum install golang -y  &>>${log_file}
+
+   app_presetup
+
+   echo -e "${color} Install Application Dependencies ${nocolor}"
+   go mod init ${component}  &>>${log_file}
+   go get  &>>${log_file}
+   go build  &>>${log_file}
 
    systemd_setup
 }
