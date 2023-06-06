fcdd5bf1ad9d86ba226b6445d5f8f22ca52a8643
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 16:56:24 +0530

    roboshop common script

--- common.sh
+++ common.sh
@@ -46,9 +46,7 @@ app_presetup() {
 systemd_setup() {
     echo -e "${color} Setup Systemd Service ${nocolor}"
     cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>${log_file}
-    stat_check $?
-
-    sed -i -e "s/roboshop_app_password/$roboshop_app_password"  /etc/systemd/system/${component}.service  &>>${log_file}
+    sed -i -e "s/roboshop_app_password/$roboshop_app_password"  /etc/systemd/system/${component}.service
     stat_check $?
 
     echo -e "${color} Start ${component} Service ${nocolor}"
