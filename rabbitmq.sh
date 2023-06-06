5b7a51f1e9023ea21b84ac5fd0bdfc8d9ee2e48b
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 16:42:31 +0530

    roboshop rabbitmq script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -18,6 +18,6 @@ systemctl restart rabbitmq-server  &>>${log_file}
 stat_check $?
 
 echo -e "${color} Add RabbitMQ Application User ${nocolor}"
-rabbitmqctl add_user roboshop roboshop123  &>>${log_file}
+rabbitmqctl add_user roboshop $1  &>>${log_file}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${log_file}
 stat_check $?
