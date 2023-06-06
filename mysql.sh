1545d5c55ca591ddab00a444564a682783a96d4b
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 16:38:05 +0530

    roboshop mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,22 @@
-echo -e "\e[35mDisable MySQL Default Version\e[0m"
-yum module disable mysql -y  &>>/tmp/roboshop.log
+source common.sh
 
-echo -e "\e[35mDisable MySQL Default Version\e[0m"
-cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo   &>>/tmp/roboshop.log
+echo -e "${color} Disable MySQL Default Version ${nocolor}"
+yum module disable mysql -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mInstall MySQL Community Server\e[0m"
-yum install mysql-community-server -y  &>>/tmp/roboshop.log
+echo -e "${color} Disable MySQL Default Version ${nocolor}"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo   &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mStart MySQL Service\e[0m"
-systemctl enable mysqld  &>>/tmp/roboshop.log
-systemctl start mysqld  &>>/tmp/roboshop.log
+echo -e "${color} Install MySQL Community Server ${nocolor}"
+yum install mysql-community-server -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mSetup MySQL Password\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1  &>>/tmp/roboshop.log
+echo -e "${color} Start MySQL Service ${nocolor}"
+systemctl enable mysqld  &>>${log_file}
+systemctl start mysqld  &>>${log_file}
+stat_check $?
+
+echo -e "${color} Setup MySQL Password ${nocolor}"
+mysql_secure_installation --set-root-pass $1  &>>${log_file}
+stat_check $?
