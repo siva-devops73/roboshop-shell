4ef4799601059c5c8b69500af4e2d3fceaa45a9b
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 17:09:08 +0530

    roboshop redis script

--- redis.sh
+++ redis.sh
@@ -1,15 +1,22 @@
-echo -e "\e[35mInstall Redis Repos\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>/tmp/roboshop.log
+source common.sh
 
-echo -e "\e[35mEnable Redis 6 version\e[0m"
-yum module enable redis:remi-6.2 -y  &>>/tmp/roboshop.log
+echo -e ${color} Install Redis Repos ${nocolor}"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mInstall Redis\e[0m"
-yum install redis -y  &>>/tmp/roboshop.log
+echo -e "${color} Enable Redis 6 version ${nocolor}"
+yum module enable redis:remi-6.2 -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mUpdate Redis Listen Address\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf  &>>/tmp/roboshop.log
+echo -e "${color} Install Redis ${nocolor}"
+yum install redis -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[35mStart Redis Service\e[0m"
-systemctl enable redis  &>>/tmp/roboshop.log
-systemctl restart redis  &>>/tmp/roboshop.log
+echo -e "${color} Update Redis Listen Address ${nocolor}"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf  &>>${log_file}
+stat_check $?
+
+echo -e "${color} Start Redis Service ${nocolor}"
+systemctl enable redis  &>>${log_file}
+systemctl restart redis  &>>${log_file}
+stat_check $?
