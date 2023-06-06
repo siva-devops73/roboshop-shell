32a5497af46e78056eb810c825ea6389461235e6
Siva Kumar <sivakumar9399959803@gmail.com>
2023-06-06 16:23:16 +0530

    roboshop common script

--- common.sh
+++ common.sh
@@ -2,34 +2,60 @@ color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+user_id=$(id -u)
+if [ $user_id -ne 0 ]; then
+   echo Script should be running with sudo
+   exit 1
+fi
 
+stat_check() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    exit 1
+  fi
+}
 
 app_presetup() {
     echo -e "${color} Add Application User ${nocolor}"
     useradd roboshop  &>>${log_file}
+    if [ $? -eq 1 ]; then
+      useradd roboshop  &>>${log_file}
+    fi
+    stat_check $?
 
     echo -e "${color} Create Application Directory ${nocolor}"
     rm -rf ${app_path}  &>>${log_file}
     mkdir ${app_path}  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Download Application Content ${nocolor}"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log_file}
     cd ${app_path}  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Extract Application Content ${nocolor}"
     cd ${app_path}  &>>${log_file}
     unzip /tmp/${component}.zip  &>>${log_file}
+    stat_check $?
 
   }
+
 
 systemd_setup() {
     echo -e "${color} Setup Systemd Service ${nocolor}"
     cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>${log_file}
+    stat_check $?
+
+    sed -i -e "s/roboshop_app_password/$roboshop_app_password"  /etc/systemd/system/${component}.service  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Start ${component} Service ${nocolor}"
     systemctl daemon-reload   &>>${log_file}
     systemctl enable ${component}  &>>${log_file}
     systemctl restart ${component}  &>>${log_file}
+    stat_check $?
 
   }
 
@@ -37,14 +63,17 @@ systemd_setup() {
 nodejs() {
     echo -e "${color} Configuring NodeJS Repos ${nocolor}"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Install NodeJS ${nocolor}"
     yum install nodejs -y  &>>${log_file}
+    stat_check $?
 
     app_presetup
 
     echo -e "${color} Install NodeJS Dependencies ${nocolor}"
     npm install  &>>${log_file}
+    stat_check $?
 
     systemd_setup
 
@@ -53,32 +82,38 @@ nodejs() {
 mongo_schema_setup() {
     echo -e "${color} Copy Mongo Repo file ${nocolor}"
     cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Install MongoDB Client ${nocolor}"
     yum install mongodb-org-shell -y  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Load Schema ${nocolor}"
     mongo --host mongodb-dev.devops73.store <${app_path}/schema/${component}.js  &>>${log_file}
+    stat_check $?
 }
 
 mysql_schema_setup() {
     echo -e "${color} Install MySQL Client ${nocolor}"
     yum install mysql -y  &>>${log_file}
+    stat_check $?
 
     echo -e "${color} Load Schema ${nocolor}"
-    mysql -h mysql-dev.devops73.store -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql   &>>${log_file}
-
+    mysql -h mysql-dev.devops73.store -uroot -p${mysql_root_password} < ${app_path}/schema/${component}.sql   &>>${log_file}
+    stat_check $?
 }
 
 maven() {
    echo -e "${color} Install Maven ${nocolor}"
    yum install maven -y  &>>${log_file}
+   stat_check $?
 
    app_presetup
 
    echo -e "${color} Download Maven Dependencies ${nocolor}"
    mvn clean package  &>>${log_file}
    mv target/${component}-1.0.jar ${component}.jar  &>>${log_file}
+   stat_check $?
 
    mysql_schema_setup
 
@@ -89,12 +124,14 @@ maven() {
 python() {
    echo -e "${color} Install Python ${nocolor}"
    yum install python36 gcc python3-devel -y  &>>${log_file}
+   stat_check $?
 
    app_presetup
 
    echo -e "${color} Install Application Dependencies ${nocolor}"
    cd ${app_path}  &>>${log_file}
    pip3.6 install -r requirements.txt  &>>${log_file}
+   stat_check $?
 
    systemd_setup
 }
@@ -102,6 +139,7 @@ python() {
 golang() {
    echo -e "${color} Install Golang ${nocolor}"
    yum install golang -y  &>>${log_file}
+   stat_check $?
 
    app_presetup
 
@@ -109,6 +147,7 @@ golang() {
    go mod init ${component}  &>>${log_file}
    go get  &>>${log_file}
    go build  &>>${log_file}
+   stat_check $?
 
    systemd_setup
 }
